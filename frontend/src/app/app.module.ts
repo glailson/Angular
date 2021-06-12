5de55cb3e1c8ebbe28365014a253113f72bebf18
Glailson Leoncio <glailson.al@gmail.com>
2021-06-12 10:11:04 -0300

    Criação do método de obter produto

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -26,7 +26,8 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { FormsModule } from '@angular/forms'
 import { MatFormFieldModule } from '@angular/material/form-field'
-import { MatInputModule } from '@angular/material/input'
+import { MatInputModule } from '@angular/material/input';
+import { ProductReadComponent } from './components/product/product-read/product-read.component'
 
 
 
@@ -40,7 +41,8 @@ import { MatInputModule } from '@angular/material/input'
     ProductCrudComponent,
     RedDirective,
     ForDirective,
-    ProductCreateComponent
+    ProductCreateComponent,
+    ProductReadComponent
     
   ],
   imports: [
